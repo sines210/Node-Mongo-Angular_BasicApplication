e38796a92109a3439f4fa187b9e05c5b29fd84ed
sines210 <sines210@gmail.com>
2021-07-09 18:07:05 +0200

    auth token

--- go-fullstack/backend/controllers/user.js
+++ go-fullstack/backend/controllers/user.js
@@ -1,7 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwtoken = require('jsonwebtoken');
 
-exports.signup((req, res, next)=>{
+exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.passord, 10)    //ici 10 représente le salt du haschage soit combien de fois l'algorithme hashe le mp 10 c'est bien après plus c elevé plus c sécurisé
     .then(hash=>{
         const user = new User({
@@ -9,17 +10,19 @@ exports.signup((req, res, next)=>{
             password: hash
         });
         user.save()
-        .then(()=>{res.status(201).json({ message: 'Utilisateur créé' })})
-        .catch(error=>res.status(500).json({ error }))
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error=>res.status(400).json({ error }))
     })
     .catch(error=>res.status(500).json({ error }))
-})
+}
 
-exports.login((req, res, next)=>{
+
+
+exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
     .then(user=>{
         if(!user){
-            return res.status(401).json({ error: 'Utilisateur non trouvé' })
+            return res.status(401).json({message: 'Utilisateur non trouvé' })
         }
         bcrypt.compare(req.body.password, user.password)
         .then(valid=>{
@@ -28,10 +31,14 @@ exports.login((req, res, next)=>{
             }
             res.status(200).json({
                 userId: user._id, 
-                token: 'TOKEN'
+                token: jwtoken.sign(
+                    {userId: user._id},
+                    'RANDOM_TOKEN_SECRET',
+                    {expiresIn: '24h'}
+                )
             })
         })
         .catch(error=>res.status(500).json({ error }))
     })
     .catch(error=>res.status(500).json({ error }))
-})
+}
